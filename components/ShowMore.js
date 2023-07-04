b4b702295642da951783e23d15eb64da643e8ed0
DongTuna <dongtuna@MacBook-Pro-DongTuna.local>
2023-07-04 10:14:12 +0700

    push all source model: server side rendering

---
+++ components/ShowMore.js
@@ -0,0 +1,32 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+import CustomButton from "./CustomButton";
+import { updateSearchParams } from "@/utils";
+
+const ShowMore = ({pageNumber,isNext,}) => {
+    const router = useRouter();
+
+    const handleNavigation = () =>{
+        const newLimit = (pageNumber + 1)*10;
+        const newPathName = updateSearchParams("limit",`${newLimit}`);
+        router.push(newPathName);
+    }
+  return (
+    <div className="w-full flex-center mt-10 gap-5">
+
+        {
+            !isNext && (
+                <CustomButton
+                    title='Show More'
+                    btnType="button"
+                    containerStyle='bg-primary-blue rounded-full text-white '
+                    handleClick={handleNavigation}
+                />
+            )
+        }
+    </div>
+  )
+}
+
+export default ShowMore
